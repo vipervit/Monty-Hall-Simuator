cb1d17f819aa4f87c9413d5de09ed56efb8405b1
Viper Vit <vitolg1@gmail.com>
2021-01-05 22:27:40 -0500

    rework 1

--- __init__.py
+++ __init__.py
@@ -9,23 +9,35 @@ logger.addHandler(console)
 logger.setLevel(logging.INFO)
 
 
-class statuses(Enum):
-    win = 'CAR'
-    loss = 'goat'
-    open = 'open'
-    guessed = 'guessed'
+class door():
+    id = None
+    hasPrize = False
+    isOpen = False
+    markedGuessed = False
 
-def setup_doors(num_of_doors=3):
+class prize(Enum):
+    win = 'CAR!'
+    bust = 'GOAT'
+
+def get_prize_doors(num_of_doors=3, num_of_prize_doors=1):
     doors = {}
     for i in range(num_of_doors):
-        doors.update({i: statuses.loss})
-    doors[random.choice(list(doors))] = statuses.win
+        doors.update({i: prize.bust})
+    while list(doors.values()).count(prize.win) < num_of_prize_doors:
+        doors[random.choice(range(len(doors)))] = random.choice([prize.win, prize.bust])
     return doors
 
-def make_guess(doors):
-    doors[random.choice(list(doors))] = statuses.guessed
-    return doors
-
-def open_losing_door(doors, guessed):
-    doors[random.choice([x for x in list(doors) if doors[x] is not statuses.win and x is not guessed])] = statuses.open
-    return doors
+# def setup_doors(num_of_doors=3):
+#     doors = []
+#     for i in range(num_of_doors):
+#         door.id = i
+#         for j in range(num_of_prize_doors):
+#             door.hasPrize = random.choice()
+#
+# def make_guess(doors):
+#     doors[random.choice(list(doors))] = statuses.guessed
+#     return doors
+#
+# def open_losing_door(doors, guessed):
+#     doors[random.choice([x for x in list(doors) if doors[x] is not statuses.win and x is not guessed])] = statuses.open
+#     return doors
--- tests/test_doors.py
+++ tests/test_doors.py
@@ -1,34 +1,45 @@
 import montyhall
-from montyhall import statuses, setup_doors, make_guess
+from montyhall import *
 
-def test_setup_doors():
-    iterations = 100000
-    win_counter, loss_counter = 0, 0
-    for i in range(iterations):
-        doors = setup_doors()
-        if doors[0] == statuses.win:
-            win_counter += 1
-        if doors[0] == statuses.loss:
-            loss_counter += 1
-    win_rate = round(win_counter/iterations,2)
-    loss_rate = round(loss_counter/iterations,2)
-    assert win_rate >= 0.32 and win_rate <= 0.34, 'Wrong statistics for winnning doors.'
-    assert  loss_rate >= 0.66 and loss_rate <= 0.68, 'Wrong statistics for losing doors.'
+def test_get_prize_doors():
+    for num_of_doors in range(3,10):
+        num_of_prize_doors = num_of_doors - 2
+        if num_of_doors == 3:
+            d = get_prize_doors()
+        else:
+            d = get_prize_doors(num_of_doors = num_of_doors, num_of_prize_doors = num_of_prize_doors)
+        assert len(d) == num_of_doors, 'Wrong size of the returned dictionary.'
+        assert list(d.values()).count(prize.win) == num_of_prize_doors, 'Wrong count of prizes.'
+        assert list(d.values()).count(prize.bust) == num_of_doors - num_of_prize_doors, 'Wrong count of losing doors.'
 
-def test_make_guess():
-    iterations = 100000
-    ct_1, ct_2, ct_3 = 0,0,0
-    for i in range(iterations):
-        doors = make_guess(setup_doors())
-        if doors[0] == statuses.guessed:
-            ct_1 += 1
-        if doors[1] == statuses.guessed:
-            ct_2 += 1
-        if doors[2] == statuses.guessed:
-            ct_3 += 1
-        rate1 = round(ct_1/iterations)
-        rate2 = round(ct_2/iterations)
-        rate3 = round(ct_3/iterations)
-        assert rate1 >= 0.32 and rate1 <= 0.33, 'Wrong statistics for first door.'
-        assert rate2 >= 0.32 and rate2 <= 0.33, 'Wrong statistics for second door.'
-        assert rate3 >= 0.32 and rate3 <= 0.33, 'Wrong statistics for third door.'
+# def test_setup_doors():
+#     iterations = 100000
+#     win_counter, loss_counter = 0, 0
+#     for i in range(iterations):
+#         doors = setup_doors()
+#         if doors[0] == statuses.win:
+#             win_counter += 1
+#         if doors[0] == statuses.loss:
+#             loss_counter += 1
+#     win_rate = round(win_counter/iterations,2)
+#     loss_rate = round(loss_counter/iterations,2)
+#     assert win_rate >= 0.32 and win_rate <= 0.34, 'Wrong statistics for winnning doors.'
+#     assert  loss_rate >= 0.66 and loss_rate <= 0.68, 'Wrong statistics for losing doors.'
+#
+# def test_make_guess():
+#     iterations = 100000
+#     ct_1, ct_2, ct_3 = 0,0,0
+#     for i in range(iterations):
+#         doors = make_guess(setup_doors())
+#         if doors[0] == statuses.guessed:
+#             ct_1 += 1
+#         if doors[1] == statuses.guessed:
+#             ct_2 += 1
+#         if doors[2] == statuses.guessed:
+#             ct_3 += 1
+#         rate1 = round(ct_1/iterations)
+#         rate2 = round(ct_2/iterations)
+#         rate3 = round(ct_3/iterations)
+#         assert rate1 >= 0.32 and rate1 <= 0.33, 'Wrong statistics for first door.'
+#         assert rate2 >= 0.32 and rate2 <= 0.33, 'Wrong statistics for second door.'
+#         assert rate3 >= 0.32 and rate3 <= 0.33, 'Wrong statistics for third door.'
